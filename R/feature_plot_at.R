7fb1b68077954c98faf3d631be1029f0e3b43f3d
Hsu-Che-Wei <44936028+Hsu-Che-Wei@users.noreply.github.com>
2022-04-06 23:29:52 +0200

    Update feature_plot_at.R

--- R/feature_plot_at.R
+++ R/feature_plot_at.R
@@ -31,7 +31,7 @@ feature_plot_at <- function(seu,res,doublet.rate,dir_to_color_scheme,save){
   p8 <- DimPlot(seu, reduction = "umap", group.by = colnames(seu@meta.data)[grep("DF.classifications",colnames(seu@meta.data))], order = c("Doublet","Singlet"),cols = c("#cccccc", "#ff4040"))+ggtitle(paste0("Doublet Rate ",doublet.rate," %"))+theme(plot.margin = unit(c(2.4,0,0.8,0), "cm"),plot.title = element_text(hjust = 0,vjust=2,size= 22, face="plain"));
   p9 <- DimPlot(seu, reduction = "umap", label = TRUE)+ggtitle(paste0("Clustering (resolution ",res,")"))+theme(legend.position="none",plot.margin = unit(c(2.4,0,0.8,4), "cm"),plot.title = element_text(hjust = 0,vjust=2,size= 22, face="plain"));
   gl <- lapply(list(p1, p2, p3, p4, p5, p6, p7, p8, p9), ggplotGrob)
-  gwidth <- do.call(unit.pmax, lapply(gl, "[[", "widths"))
+  gwidth <- do.call(grid::unit.pmax, lapply(gl, "[[", "widths"))
   gl <- lapply(gl, "[[<-", "widths", value = gwidth)
 
   png(save, width = 22, height = 40, units = 'in', res=300)
