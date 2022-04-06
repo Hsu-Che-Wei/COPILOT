12d98fb18a96fbaaccac9e5f536df005dd6be360
Hsu-Che-Wei <44936028+Hsu-Che-Wei@users.noreply.github.com>
2022-04-06 23:29:20 +0200

    Update feature_plot.R

--- R/feature_plot.R
+++ R/feature_plot.R
@@ -21,7 +21,7 @@ feature_plot <- function(seu,res,doublet.rate,save){
   p4 <- DimPlot(seu, reduction = "umap", group.by = colnames(seu@meta.data)[grep("DF.classifications",colnames(seu@meta.data))], order = c("Doublet","Singlet"),cols = c("#cccccc", "#ff4040"))+ggtitle(paste0("Doublet Rate ",doublet.rate," %"))+theme(plot.margin = unit(c(2.4,0,0.8,0.8), "cm"),plot.title = element_text(hjust = 0,vjust=2,size= 22, face="plain"));
   p5 <- DimPlot(seu, reduction = "umap", label = TRUE)+ggtitle(paste0("Clustering (resolution ",res,")"))+theme(legend.position="none",plot.margin = unit(c(2.4,0.8,0.8,4), "cm"),plot.title = element_text(hjust = 0,vjust=2,size= 22, face="plain"));
   gl <- lapply(list(p1, p2, p3, p4, p5), ggplotGrob)
-  gwidth <- do.call(unit.pmax, lapply(gl, "[[", "widths"))
+  gwidth <- do.call(grid::unit.pmax, lapply(gl, "[[", "widths"))
   gl <- lapply(gl, "[[<-", "widths", value = gwidth)
 
   png(save, width = 22, height = 24, units = 'in', res=300)
